6d7d1174f41015dc723fb7f0421847b4a88e2fbb
Your Name <you@example.com>
2024-08-14 22:53:38 -0500

    Version 2 React Native

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen />
+        name=""
+      </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
